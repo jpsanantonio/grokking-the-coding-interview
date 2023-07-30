7f9a179b5d6eb105ae6f9d4cafdb4a9765b0fc57
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-07-30 00:06:20 -0700

    Pangram

---
+++ pangram.js
@@ -0,0 +1,35 @@
+function isPangram(string) {
+  const set = new Set();
+
+  for (const char of string) {
+    if (char.match(/[a-z]/i)) {
+      set.add(char.toLowerCase());
+    }
+  }
+
+  if (set.size === 26) {
+    return true;
+  }
+
+  return false;
+}
+
+isPangram('TheQuickBrownFoxJumpsOverTheLazyDog')
+isPangram('This is not a pangram')function isPangram(string) {
+  const set = new Set();
+
+  for (const char of string) {
+    if (char.match(/[a-z]/i)) {
+      set.add(char.toLowerCase());
+    }
+  }
+
+  if (set.size === 26) {
+    return true;
+  }
+
+  return false;
+}
+
+isPangram('TheQuickBrownFoxJumpsOverTheLazyDog')
+isPangram('This is not a pangram')
