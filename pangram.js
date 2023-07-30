0da63d26f29f8e80dbee0556143f18c221a0b9a8
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-07-30 12:53:36 -0700

    Fix Pangram

--- pangram.js
+++ pangram.js
@@ -15,21 +15,4 @@ function isPangram(string) {
 }
 
 isPangram('TheQuickBrownFoxJumpsOverTheLazyDog')
-isPangram('This is not a pangram')function isPangram(string) {
-  const set = new Set();
-
-  for (const char of string) {
-    if (char.match(/[a-z]/i)) {
-      set.add(char.toLowerCase());
-    }
-  }
-
-  if (set.size === 26) {
-    return true;
-  }
-
-  return false;
-}
-
-isPangram('TheQuickBrownFoxJumpsOverTheLazyDog')
 isPangram('This is not a pangram')
